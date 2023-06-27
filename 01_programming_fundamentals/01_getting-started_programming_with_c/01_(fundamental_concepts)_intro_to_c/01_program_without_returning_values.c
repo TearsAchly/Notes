8f3e2a0adba7265eeeae696c552e38095481e9f0
TearsAchly <indrawans325@gmail.com>
2023-06-27 09:00:00 +0700

    feat: add simple C program that prints a message
    - Created a new program that displays "Hello Mom!" on the console without returning a value from `main`.

---
+++ 01_programming_fundamentals/01_getting-started_programming_with_c/01_(fundamental_concepts)_intro_to_c/01_program_without_returning_values.c
@@ -0,0 +1,23 @@
+/*
+ * Program Title: Simple Program Without Returning Values
+ * Author: Achly
+ * Date: June 2023
+ * Description: This program prints a simple message to the console without returning any values from the `main` function.
+ *
+ * Logic: The program uses `printf` to display a message on the console.
+ * Preconditions: None
+ * Postconditions: A message is displayed on the console.
+ *
+ * Program Flow:
+ * 1. The program starts with the `main` function.
+ * 2. The `printf` function is used to print the message "Hello Mom!" to the console.
+ * 3. The program ends after the message is displayed.
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    // Print a simple message to the console
+    printf("Hello Mom!"); // Display the message
+    return 0; // Indicate successful completion
+}
