41f60a082fdc0cb2bbed8428adefe85c97f75e41
TearsAchly <indrawans325@gmail.com>
2023-08-06 11:00:00 +0700

    refactor: Add loop for menu and exit option in main program
    - Implemented a loop in the main function to allow the user to repeatedly access the menu.
    - Added an exit option to terminate the program cleanly.

--- 01_programming_fundamentals/01_getting-started_programming_with_c/07_(development_projects)/01_ticket_booking_system/main/index.c
+++ 01_programming_fundamentals/01_getting-started_programming_with_c/07_(development_projects)/01_ticket_booking_system/main/index.c
@@ -1,24 +1,30 @@
 #include "../include/available_seats.h"
 #include "../include/ticket.h"
+#include <stdbool.h>
 #include <stdio.h>
 
 int main() {
+  int loop = true;
+  while (loop) {
+    int chose;
+    printf("\n\nmenu\n1.check available seats.\n2.order tickets.\n3.exit\n\n");
+    scanf("%d", &chose);
 
-  int chose;
-  printf("menu\n0.check available seats.\n1.order tickets.\n\n");
-  scanf("%d", &chose);
-
-  switch (chose) {
-  case 0:
-    available_seats();
-    break;
-  case 1:
-    create_ticket();
-    result_ticket();
-    break;
-  default:
-    printf("invalid input");
+    switch (chose) {
+    case 1:
+      available_seats();
+      break;
+    case 2:
+      create_ticket();
+      result_ticket();
+      break;
+    case 3:
+      loop = !true;
+      printf("\nexit");
+      break;
+    default:
+      printf("\ninvalid input");
+    }
   }
-
   return 0;
 }
