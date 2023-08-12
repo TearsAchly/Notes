b3e97732a43d914fc458a83f93a6c5d51d4ef134
TearsAchly <indrawans325@gmail.com>
2023-08-12 08:00:00 +0700

    feat: Add simple Java program to print a personalized message
    - Implemented a `Main` class under `src` package that prints "Hello mom" to the console.
    - Demonstrates the basic structure of a Java program with a `main` method and `System.out.println`.

---
+++ 01_programming_fundamentals/02_getting-started_programming_with_java/01_building_java_projects/src/Main.java
@@ -0,0 +1,34 @@
+/*
+ * Program Title: Simple Java Program Displaying a Custom Message
+ * Author: Achly
+ * Date: August 2023
+ * Description: This Java program prints a personalized message, "Hello mom," to the console.
+ *              It is structured within a basic project layout that includes source and compiled directories.
+ *
+ * Project Structure:
+ * .
+ * |-- Main.class     // Compiled bytecode file
+ * `-- src
+ *     `-- Main.java  // Source code file
+ *
+ * Logic: The program uses `System.out.println` to display the custom message on the console.
+ * Preconditions: None
+ * Postconditions: The message "Hello mom" is displayed on the console.
+ *
+ * Program Flow:
+ * 1. The program starts execution from the `main` method, which is the entry point for Java programs.
+ * 2. The `System.out.println` method is called to print the message "Hello mom" to the console.
+ * 3. The program completes execution after the message is displayed, with no further instructions.
+ *
+ * Notes:
+ * - The `Main.java` source file is located in the `src` directory.
+ * - After compilation, the generated `Main.class` file is used for execution.
+ */
+
+public class Main {
+  public static void main(String[] args) {
+    // Print a personalized message to the console
+    System.out.println("Hello mom"); // Display the message "Hello mom"
+  }
+}
+
